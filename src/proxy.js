f0b67fa84d9a8cea10e6b25b48930b2b2e119a45
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2026-09-21 18:39:24 +0200

    weird proxy problem

--- src/proxy.js
+++ src/proxy.js
@@ -2,13 +2,31 @@ const http = require('http');
 const https = require('https');
 const httpProxy = require('http-proxy');
 
-const ipv6HttpsAgent = new https.Agent({ family: 6, keepAlive: true });
-const ipv6HttpAgent = new http.Agent({ family: 6, keepAlive: true });
+const hopHost = String(process.env.FLOW_HOP_HOST || '').trim();
+const hopPort = Number(process.env.FLOW_HOP_PORT || 4180);
 
 function isFlowHostname(hostname) {
   return hostname === 'flow.hands-on-technology.org'
     || hostname.endsWith('.flow.hands-on-technology.org');
 }
+
+function createHopAgent(BaseAgent) {
+  const agent = new BaseAgent({ keepAlive: true });
+  const connect = BaseAgent.prototype.createConnection;
+  agent.createConnection = function hopConnection(options, callback) {
+    if (!options.servername) {
+      options.servername = options.hostname || options.host;
+    }
+    options.host = hopHost;
+    options.hostname = hopHost;
+    options.port = hopPort;
+    return connect.call(this, options, callback);
+  };
+  return agent;
+}
+
+const hopHttpsAgent = hopHost ? createHopAgent(https.Agent) : new https.Agent({ family: 6, keepAlive: true });
+const hopHttpAgent = hopHost ? createHopAgent(http.Agent) : new http.Agent({ family: 6, keepAlive: true });
 
 function agentFor(target) {
   try {
@@ -16,7 +34,7 @@ function agentFor(target) {
   } catch {
     return undefined;
   }
-  return /^https:/i.test(target) ? ipv6HttpsAgent : ipv6HttpAgent;
+  return /^https:/i.test(target) ? hopHttpsAgent : hopHttpAgent;
 }
 
 function describeError(error) {
